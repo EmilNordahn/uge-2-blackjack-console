2c766cf5852d2b2f91757265e7ac5e0b7871ef6d
EmilNordahn <60731699+EmilNordahn@users.noreply.github.com>
2025-09-24 14:07:32 +0200

    Add toString override
    Also removed unused Value property

--- blackjack-console/Card.cs
+++ blackjack-console/Card.cs
@@ -1,7 +1,5 @@
 class Card
 {
-    //Value is the value of the card, face cards (kings, etc) are all worth 10, aces are either 1 or 11, depending on the rest of the hand
-    public int Value { get; set; }
     //Suit represents the suit of the card, 1-4 (1 is hearts, 2 is diamonds, 3 is clubs, 4 is spades)
     public int Suit { get; set; }
     //Face represents the number on the card, 1-13 (1 is ace, 11 is jack, 12 is queen, 13 is king)
@@ -20,7 +18,7 @@ class Card
         foreach (Card card in cards)
         {
             if (card.Face > 1 && card.Face < 11)
-                total += card.Value;
+                total += card.Face;
             // Face cards (jack, queen, king) are worth 10
             else if (card.Face >= 11)
             {
@@ -36,5 +34,12 @@ class Card
             }
         }
         return total;
+    }
+
+    public override string ToString()
+    {
+        string[] faces = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+        string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+        return $"{faces[Face - 1]} of {suits[Suit - 1]}";
     }
 }
